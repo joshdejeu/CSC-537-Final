e5fb6f8bf66477d4fd4a5fef7efd55b895c3adf6
John Smith <joshplays21@gmail.com>
2025-04-10 15:33:54 -0500

    switched from Detectron2 to Ultralytics YOLOv8

---
+++ train/train_baseline.py
@@ -0,0 +1,19 @@
+# ============================
+# Trains the baseline model (to compare later)
+# ============================
+
+from ultralytics import YOLO
+
+# Load a pretrained YOLOv8 segmentation model
+model = YOLO("yolov8s-seg.pt")
+
+# Train the model
+# Note: To save last.pt it must run for atleast 1 epoch.
+model.train(
+    data="datasets/mju.yaml",
+    epochs=50,
+    imgsz=640,
+    name="baseline_mju"
+)
+
+
