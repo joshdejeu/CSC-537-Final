647baf2c2b23c250f49c004f999a6d2145ff926e
joshdejeu <dejeujosh@gmail.com>
2025-04-19 00:52:33 -0500

    req clean up

--- models/matterport/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='mask-rcnn',
-    version='2.1',
-    description='Matterport Mask R-CNN',
-    packages=find_packages(),  # Automatically finds mrcnn
-    install_requires=[],
-)
